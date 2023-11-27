b99fdcaecbe9760c272c584f13e8770d047688cc
Soham Sarkar <soham.ss1@gmail.com>
2023-11-27 20:28:45 +0530

    feat: add spline scene for custom 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import tailwind from '@astrojs/tailwind';
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import {defineConfig} from 'astro/config'
+
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://sohamsarkar.com',
-  integrations: [tailwind(), sitemap()]
-});
+  site: 'https://sohamsarkar.com',
+  integrations: [tailwind(), sitemap(), react({include: ['**/react/*']})],
+})
---
+++ src/components/react/spline404.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline'
+
+export default function Spline404() {
+  return (
+    <div>
+      <Spline scene='https://prod.spline.design/cIwKoprEugD8hxnL/scene.splinecode' />
+    </div>
+  )
+}
