e0dc39d6f880e90d9ea8cd8539f65b7aa6074f32
Soham Sarkar <soham.ss1@gmail.com>
2024-10-10 12:03:59 +0530

    feat: home page v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,8 @@
-import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 import {defineConfig} from 'astro/config'
-import icon from 'astro-icon'
-
-import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sohamsarkar.com',
-  integrations: [
-    tailwind(),
-    sitemap(),
-    react({include: ['**/react/*']}),
-    icon({
-      include: {
-        // Include only three `mdi` icons in the bundle
-        // mdi: ['account', 'account-plus', 'account-minus'],
-        ri: ['*'],
-      },
-    }),
-  ],
+  integrations: [sitemap()],
 })
